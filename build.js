26e04535ad16958e8a3979118aa23cda053d3051
余阿伟 <awei.yu@2zhuzi.com>
2016-09-17 19:08:10 +0800

    发布

---
+++ build.js
@@ -0,0 +1,57 @@
+/**
+ * Created by aweiu on 16/9/16.
+ */
+var babel = require('babel-core')
+var path = require('path')
+var exec = require('child_process').exec
+var fs = require('fs')
+var config = eval(fs.readFileSync(process.cwd() + '/build.js', 'utf8'))
+var ignore = config.ignore
+var others = config.others
+function translate (ff, ignore) {
+  ignore = ignore || []
+  if (ignore.indexOf(ff) !== -1) return
+  var files = fs.readdirSync(ff)
+  for (var fn in files) {
+    var fPath = ff + path.sep + files[fn]
+    if (ignore.indexOf(fPath) !== -1) continue
+    var stat = fs.lstatSync(fPath)
+    if (stat.isDirectory()) translate(fPath, ignore)
+    else if (files[fn].substring(files[fn].length - 3) === '.js') {
+      var dir = './dist/' + fPath
+      if (mkdirsSync(dir)) fs.writeFileSync(dir, babel.transformFileSync(fPath).code)
+    }
+  }
+}
+function mkdirsSync (dirpath, mode) {
+  if (!fs.existsSync(dirpath)) {
+    try {
+      var pathtmp
+      dirpath = dirpath.split(path.sep)
+      for (var i = 0, l = dirpath.length - 1; i < l; i++) {
+        pathtmp = pathtmp ? path.join(pathtmp, dirpath[i]) : dirpath[i]
+        if (!fs.existsSync(pathtmp)) fs.mkdirSync(pathtmp, mode)
+      }
+    } catch (e) {
+      return false
+    }
+  }
+  return true
+}
+function addOtherFile (options) {
+  for (var fPath in options) {
+    var str = fs.readFileSync(fPath, 'utf8')
+    var strFun = options[fPath]
+    var dir = './dist/' + fPath
+    if (mkdirsSync(dir)) fs.writeFileSync(dir, typeof strFun === 'function' ? strFun(str) : str)
+  }
+}
+module.exports = function () {
+  console.log('start building...')
+  exec('rm -rf dist', function () {
+    translate('.', ignore)
+    addOtherFile(others)
+    console.log('building completed')
+  })
+}
+
